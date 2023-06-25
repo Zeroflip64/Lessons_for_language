ccbbbf7496a675d79f4cf4ec1af8a06c39df388d
Ramil <122420589+Zeroflip64@users.noreply.github.com>
2023-06-25 12:18:38 +0700

    Update untitled39.py

--- untitled39.py
+++ untitled39.py
@@ -35,86 +35,82 @@ from transformers import pipeline
 from spellchecker import SpellChecker
 import en_core_web_sm
 import streamlit as st
-@st.cache_data()
 
-def ss_features_fill_mask_data():
-  class SentenceSimilarity:
-      def __init__(self, model_name='distilbert-base-uncased'):
-          self.tokenizer = DistilBertTokenizer.from_pretrained(model_name)
-          self.model = DistilBertModel.from_pretrained(model_name)
+class SentenceSimilarity:
+  def __init__(self, model_name='distilbert-base-uncased'):
+      self.tokenizer = DistilBertTokenizer.from_pretrained(model_name)
+      self.model = DistilBertModel.from_pretrained(model_name)
 
-      def sentence_to_vec(self, sentence: str):
-          inputs = self.tokenizer(sentence, return_tensors="pt")
-          outputs = self.model(**inputs)
-          return outputs.last_hidden_state.mean(dim=1).detach().numpy().squeeze()
+  def sentence_to_vec(self, sentence: str):
+      inputs = self.tokenizer(sentence, return_tensors="pt")
+      outputs = self.model(**inputs)
+      return outputs.last_hidden_state.mean(dim=1).detach().numpy().squeeze()
 
-      def compare_sentences(self, sentence1: str, sentence2: str):
-          vec1 = self.sentence_to_vec(sentence1)
-          vec2 = self.sentence_to_vec(sentence2)
-          similarity = 1 - cosine(vec1, vec2)
-          return similarity
+  def compare_sentences(self, sentence1: str, sentence2: str):
+      vec1 = self.sentence_to_vec(sentence1)
+      vec2 = self.sentence_to_vec(sentence2)
+      similarity = 1 - cosine(vec1, vec2)
+      return similarity
 
 
 
 
-  class Features:
-      d = cmudict.dict()
-      word_freqs = FreqDist(i.lower() for i in reuters.words())
-      common_words = set(nltk_words.words())
-      stop_words = set(stopwords.words('english'))
-      nlp = spacy.load('en_core_web_sm')
+class Features:
+  d = cmudict.dict()
+  word_freqs = FreqDist(i.lower() for i in reuters.words())
+  common_words = set(nltk_words.words())
+  stop_words = set(stopwords.words('english'))
+  nlp = spacy.load('en_core_web_sm')
 
-      def __init__(self, first):
-          self.first = first
-          self.clean_text = self.clean_html(self.first)
-          self.sentences = sent_tokenize(self.clean_text)
-          words = word_tokenize(self.clean_text)
-          self.words = [word.lower() for word in words if word.isalpha()]
-          self.non_stopwords = [word for word in self.words if word not in self.stop_words]
-          self.complex_words = self.hard_words()
-          self.doc = self.nlp(self.first)
+  def __init__(self, first):
+      self.first = first
+      self.clean_text = self.clean_html(self.first)
+      self.sentences = sent_tokenize(self.clean_text)
+      words = word_tokenize(self.clean_text)
+      self.words = [word.lower() for word in words if word.isalpha()]
+      self.non_stopwords = [word for word in self.words if word not in self.stop_words]
+      self.complex_words = self.hard_words()
+      self.doc = self.nlp(self.first)
 
-      def clean_html(self, raw_html):
-          cleanr = re.compile('<.*?>')
-          intermediate_text = re.sub(cleanr, '', raw_html)
-          cleantext = intermediate_text.replace('\n', ' ')
-          return cleantext
+  def clean_html(self, raw_html):
+      cleanr = re.compile('<.*?>')
+      intermediate_text = re.sub(cleanr, '', raw_html)
+      cleantext = intermediate_text.replace('\n', ' ')
+      return cleantext
 
-      def nsyl(self, word):#слоги
-          return max([len(list(y for y in x if y[-1].isdigit())) for x in self.d.get(word.lower(), [])] or [0])
+  def nsyl(self, word):#слоги
+      return max([len(list(y for y in x if y[-1].isdigit())) for x in self.d.get(word.lower(), [])] or [0])
 
-      def hard_words(self, frequency_threshold=5000):# сложные слова которые встречаются в тексте
-          return {
-              word for word in self.non_stopwords if len(word) > 2 and word in self.d and self.nsyl(word) > 2 and self.word_freqs[word] < frequency_threshold
-          }
+  def hard_words(self, frequency_threshold=5000):# сложные слова которые встречаются в тексте
+      return {
+          word for word in self.non_stopwords if len(word) > 2 and word in self.d and self.nsyl(word) > 2 and self.word_freqs[word] < frequency_threshold
+      }
 
-      def flesch_kincaid(self):# Flesch-Kincaid grade level
-          total_sentences = len(self.sentences)
-          total_words = len(self.words)
-          total_syllables = sum([self.nsyl(word) for word in self.words])
-
-
-          if total_words and total_sentences:
-              FK_grade = 0.39 * (total_words / total_sentences) + 11.8 * (total_syllables / total_words) - 15.59
-              return FK_grade
-          else:
-              return 0
+  def flesch_kincaid(self):# Flesch-Kincaid grade level
+      total_sentences = len(self.sentences)
+      total_words = len(self.words)
+      total_syllables = sum([self.nsyl(word) for word in self.words])
 
 
+      if total_words and total_sentences:
+          FK_grade = 0.39 * (total_words / total_sentences) + 11.8 * (total_syllables / total_words) - 15.59
+          return FK_grade
+      else:
+          return 0
 
-  fill_mask = pipeline(
-      "fill-mask",
-      model="distilbert-base-multilingual-cased",
-      tokenizer="distilbert-base-multilingual-cased"
-  )
-  url = 'https://raw.githubusercontent.com/Zeroflip64/Lessons_for_language/main/sub_all.csv'
-  syb_all = pd.read_csv(url)
+
+
+fill_mask = pipeline(
+  "fill-mask",
+  model="distilbert-base-multilingual-cased",
+  tokenizer="distilbert-base-multilingual-cased"
+)
+url = 'https://raw.githubusercontent.com/Zeroflip64/Lessons_for_language/main/sub_all.csv'
+syb_all = pd.read_csv(url)
 
   
+ss=SentenceSimilarity()
 
-  return SentenceSimilarity,Features,fill_mask,syb_all
-  
-SentenceSimilarity,Features,fill_mask,syb_all=ss_features_fill_mask_data()
 
 document=None
 uploaded_file = st.file_uploader("Загрузите ваш документ", type=["txt"])
