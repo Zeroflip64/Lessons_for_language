2205c44cf340a1dca95b6adbbf6a7ed8b43a2976
Ramil <122420589+Zeroflip64@users.noreply.github.com>
2023-06-26 13:39:01 +0700

    Update untitled39.py

--- untitled39.py
+++ untitled39.py
@@ -209,8 +209,9 @@ def empty_words(df):# Упражение 1
 def sentenses_by_time(sentenses_list):  # Пропуски на правильное время глагола
   nlp = spacy.load("en_core_web_sm")
   
-  if 'selected_sentence' not in st.session_state:
+  if 'new_sentence' not in st.session_state or st.session_state.new_sentence:
     st.session_state.selected_sentence = random.choice([sent for sent in sentenses_list if len(sent.split(' ')) > 5])
+    st.session_state.new_sentence = False
 
   doc = nlp(st.session_state.selected_sentence)
   sen = []
@@ -240,7 +241,7 @@ def sentenses_by_time(sentenses_list):  # Пропуски на правильн
     mistakes = 0
     for idx, (user_verb, correct_verb) in enumerate(zip(user_verbs, correct_verbs)):
       if user_verb != correct_verb:
-        st.write(f'Ошибка верное слово {correct_verb} для пропуска {idx+1}')
+
         mistakes += 1
     if mistakes:
       st.write('Попробуйте снова')
@@ -248,10 +249,11 @@ def sentenses_by_time(sentenses_list):  # Пропуски на правильн
       st.write('Вы отлично справились')
 
     st.write(f'Количество ошибок {mistakes} из {len(user_verbs)} вариантов')
-    if st.button('Новое предложение'):
-      del st.session_state.selected_sentence
-      for idx in verbs_indices:
-        del st.session_state[f'verb{idx}']
+
+  if st.button('Новое предложение'):
+    st.session_state.new_sentence = True
+    for idx in verbs_indices:
+      del st.session_state[f'verb{idx}']
     
 st.header('Упражнение 1')
 st.subheader('Упражнение где необходимо выбрать правильное слово подходящее по смыслу')
